691892389c86aa10117595688f223b5628d7e821
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2023-08-23 13:14:24 -0300

    requisito 4 finalizado

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,19 @@
 import { useState } from 'react';
 
 function Form() {
+  const [nomeServico, setNomeServico] = useState('');
+  const [campoLogin, setCampoLogin] = useState('');
+  const [campoSenha, setCampoSenha] = useState('');
   const [formulario, setFormulario] = useState(false);
+
+  function validarCadastro() {
+    const validarCampoServico = nomeServico.length > 0;
+    const validarCampoLogin = campoLogin.length > 0;
+    const validarCampoSenha = campoSenha.length >= 8 && campoSenha.length <= 16;
+    const regex = /^(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=!])[a-z\d@#$%^&+=!]{8,16}$/;
+    return (validarCampoServico
+      && validarCampoLogin && validarCampoSenha && regex.test(campoSenha));
+  }
 
   const HandleClick = () => {
     setFormulario(!formulario);
@@ -10,34 +22,52 @@ function Form() {
   return (
     <div>
       {
-    formulario
-      ? (
-        <form action="">
-          <label htmlFor="servico">Nome do serviço</label>
-          <input type="text" name="servico" id="servico" />
+        formulario
+          ? (
+            <form action="">
+              <label htmlFor="nomeServico">Nome do serviço</label>
+              <input
+                type="text"
+                name=""
+                id="nomeServico"
+                onChange={ (e) => setNomeServico(e.target.value) }
+                value={ nomeServico }
+              />
 
-          <label htmlFor="entrar">Login</label>
-          <input type="text" name="entrar" id="entrar" />
+              <label htmlFor="campoLogin">Login</label>
+              <input
+                type="text"
+                name=""
+                id="campoLogin"
+                onChange={ (e) => setCampoLogin(e.target.value) }
+                value={ campoLogin }
+              />
 
-          <label htmlFor="chave">Senha</label>
-          <input type="password" name="chave" id="chave" />
+              <label htmlFor="campoSenha">Senha</label>
+              <input
+                type="password"
+                name=""
+                id="campoSenha"
+                onChange={ (e) => setCampoSenha(e.target.value) }
+                value={ campoSenha }
+              />
 
-          <label htmlFor="caminho">URL</label>
-          <input type="text" name="caminho" id="caminho" />
-          <button>Cadastrar</button>
-          <button onClick={ HandleClick }>Cancelar</button>
+              <label htmlFor="caminho">URL</label>
+              <input type="text" name="" id="caminho" />
 
-        </form>
-      )
-      : (
-        <div>
-          <button onClick={ HandleClick }>Cadastrar nova senha</button>
-        </div>
-      )
+              <button disabled={ !validarCadastro() }>Cadastrar</button>
+
+              <button onClick={ HandleClick }>Cancelar</button>
+            </form>
+          )
+          : (
+            <div>
+              <button onClick={ HandleClick }>Cadastrar nova senha</button>
+            </div>
+          )
    }
 
     </div>
   );
 }
-
 export default Form;
