15d17a78a8bc775469161e881ec31021d062abd0
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2023-08-24 15:48:10 -0300

    feat: Requisito 6 e 7 finalizados

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
+import { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 
+type NovaSenhaNew = {
+  nomeServico: string,
+  campoLogin:string,
+  campoSenha: string,
+  campoLink: string,
+
+};
+
 function App() {
+  const [estadoForm, setEstadoForm] = useState(false);
+  const [removeSenha, setRemoveSenha] = useState<NovaSenhaNew[]>([]);
+  const removerSenhas = (senha: NovaSenhaNew) => {
+    setRemoveSenha([...removeSenha, senha]);
+    setEstadoForm(false);
+  };
   return (
     <div>
       <h1>Gerenciador de Senhas</h1>
-      <Form />
+
+      {estadoForm ? (
+        <Form removerSenhas={ removerSenhas } setEstadoForm={ setEstadoForm } />
+      ) : (
+        <button onClick={ () => setEstadoForm(true) }>Cadastrar Nova Senha</button>
+      )}
+
+      <div>
+
+        {removeSenha.length === 0 ? <span>Nenhuma senha Cadastrada</span>
+          : removeSenha.map((senha) => (
+            <div key={ senha.campoLogin }>
+              <a href={ senha.campoLink }>{ senha.nomeServico}</a>
+              <p>{senha.campoLogin}</p>
+              <p>{senha.campoSenha}</p>
+              <button
+                data-testid="remove-btn"
+                onClick={ () => setRemoveSenha(removeSenha
+                  .filter((password) => password.campoLogin !== senha.campoLogin)) }
+
+              >
+                remover
+              </button>
+            </div>))}
+      </div>
     </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,98 +1,118 @@
 import { useState } from 'react';
 
-function Form() {
-  const [nomeServico, setNomeServico] = useState('');
-  const [campoLogin, setCampoLogin] = useState('');
-  const [campoSenha, setCampoSenha] = useState('');
-  const [formulario, setFormulario] = useState(false);
+export type NovaSenha = {
+  nomeServico: string,
+  campoLogin:string,
+  campoSenha: string,
+  campoLink: string,
+};
+
+type RemoverSenhaType = {
+  removerSenhas:(newpassword: NovaSenha) => void;
+  setEstadoForm:(estado: boolean) => void;
+};
+function Form({ removerSenhas, setEstadoForm }: RemoverSenhaType) {
+  const [infoForm, setInfoForm] = useState<NovaSenha>({
+    nomeServico: '',
+    campoLogin: '',
+    campoSenha: '',
+    campoLink: '',
+  });
+
+  function mudandoEstadoInput(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setInfoForm({ ...infoForm, [name]: value });
+  }
 
   function alertaDoFormulario(alerta: string, validacao: boolean) {
     const validarCampos = validacao ? 'valid-password-check' : 'invalid-password-check';
     return <span className={ validarCampos }>{alerta}</span>;
   }
 
-  function validarCadastro() {
-    const validarCampoServico = nomeServico.length > 0;
-    const validarCampoLogin = campoLogin.length > 0;
-    const validarCampoSenha = campoSenha.length >= 8 && campoSenha.length <= 16;
-    const regex = /^(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=!])[a-z\d@#$%^&+=!]{8,16}$/;
-    return (validarCampoServico
-      && validarCampoLogin && validarCampoSenha && regex.test(campoSenha));
+  function Cadastrar(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
   }
 
-  const HandleClick = () => {
-    setFormulario(!formulario);
-  };
+  function validarCadastro() {
+    const validarCampoServico = infoForm.nomeServico.length > 0;
+    const validarCampoLogin = infoForm.campoLogin.length > 0;
+    const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@#$%^&+=!_])[A-Za-z\d@#$%^&+=!_]{8,16}$/;
+    return !(validarCampoServico
+      && validarCampoLogin && regex.test(infoForm.campoSenha));
+  }
 
   return (
     <div>
-      {
-        formulario
-          ? (
-            <form action="">
-              <label htmlFor="nomeServico">Nome do serviço</label>
-              <input
-                type="text"
-                name=""
-                id="nomeServico"
-                onChange={ (e) => setNomeServico(e.target.value) }
-                value={ nomeServico }
-              />
 
-              <label htmlFor="campoLogin">Login</label>
-              <input
-                type="text"
-                name=""
-                id="campoLogin"
-                onChange={ (e) => setCampoLogin(e.target.value) }
-                value={ campoLogin }
-              />
+      <form onSubmit={ Cadastrar }>
+        <label htmlFor="nomeServico">Nome do serviço</label>
+        <input
+          type="text"
+          name="nomeServico"
+          id="nomeServico"
+          onChange={ mudandoEstadoInput }
+          value={ infoForm.nomeServico }
+        />
 
-              <label htmlFor="campoSenha">Senha</label>
-              <input
-                type="password"
-                name=""
-                id="campoSenha"
-                onChange={ (e) => setCampoSenha(e.target.value) }
-                value={ campoSenha }
-              />
+        <label htmlFor="campoLogin">Login</label>
+        <input
+          type="text"
+          name="campoLogin"
+          id="campoLogin"
+          onChange={ mudandoEstadoInput }
+          value={ infoForm.campoLogin }
+        />
 
-              <label htmlFor="caminho">URL</label>
-              <input type="text" name="" id="caminho" />
+        <label htmlFor="campoSenha">Senha</label>
+        <input
+          type="password"
+          name="campoSenha"
+          id="campoSenha"
+          onChange={ mudandoEstadoInput }
+          value={ infoForm.campoSenha }
+        />
 
-              <button disabled={ !validarCadastro() }>Cadastrar</button>
+        <label htmlFor="campoLink">URL</label>
+        <input
+          type="text"
+          name="campoLink"
+          id="campoLink"
+          onChange={ mudandoEstadoInput }
+          value={ infoForm.campoLink }
+        />
 
-              <button onClick={ HandleClick }>Cancelar</button>
-              <div>
-                <ol>
-                  <li>
-                    {alertaDoFormulario(
-                      'Possuir 8 ou mais caracteres',
-                      campoSenha.length >= 8,
-                    )}
-                  </li>
-                  <li>
-                    {alertaDoFormulario(
-                      'Possuir até 16 caracteres',
-                      campoSenha.length <= 16,
-                    )}
-                  </li>
-                  <li>
-                    {alertaDoFormulario('Possuir letras e números', /^(?=.*[a-z])(?=.*\d)/.test(campoSenha))}
-                  </li>
-                  <li>
-                    {alertaDoFormulario('Possuir algum caractere especial', /^(?=.*[@#$%^&+=!])/.test(campoSenha))}
-                  </li>
-                </ol>
-              </div>
-            </form>
-          )
-          : (
-            <div>
-              <button onClick={ HandleClick }>Cadastrar nova senha</button>
-            </div>
-          )
-   }
+        <button
+          disabled={ validarCadastro() }
+          onClick={ () => removerSenhas(infoForm) }
+        >
+          Cadastrar
+
+        </button>
+
+        <button onClick={ () => setEstadoForm(false) }>Cancelar</button>
+        <div>
+          <ol>
+            <li>
+              {alertaDoFormulario(
+                'Possuir 8 ou mais caracteres',
+                infoForm.campoSenha.length >= 8,
+              )}
+            </li>
+            <li>
+              {alertaDoFormulario(
+                'Possuir até 16 caracteres',
+                infoForm.campoSenha.length > 0 && infoForm.campoSenha.length <= 16,
+              )}
+            </li>
+            <li>
+              {alertaDoFormulario('Possuir letras e números', /^(?=.*[a-z])(?=.*\d)/.test(infoForm.campoSenha))}
+            </li>
+            <li>
+              {alertaDoFormulario('Possuir algum caractere especial', /^(?=.*[@#$%^&+=!])/.test(infoForm.campoSenha))}
+            </li>
+          </ol>
+        </div>
+      </form>
 
     </div>
   );
